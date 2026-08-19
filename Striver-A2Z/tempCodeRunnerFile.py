0fae4695b8a2f3451a2fe188a1412084916cbcbe
KOUSHIKEYA-VADIKA <koushikeyav@gmail.com>
2026-08-19 07:24:54 +0530

    optimised

--- Striver-A2Z/tempCodeRunnerFile.py
+++ Striver-A2Z/tempCodeRunnerFile.py
@@ -1,12 +1,15 @@
 num=int(input())
 reverse=0
-sign=1
+original=0
 if(num<0):
+    original=num
     sign=-1
     num=num*sign
 while(num>0):
     lastdigit=num%10
     reverse=(reverse*10)+lastdigit
     num//=10
-reverse=reverse*sign
+
+if(original<0):
+    reverse=reverse*sign
 print(reverse)
