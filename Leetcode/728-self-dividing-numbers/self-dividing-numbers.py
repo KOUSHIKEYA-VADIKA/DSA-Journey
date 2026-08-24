202a43f82dbb3a704c6ccca078417ccefecbdd46
KOUSHIKEYA VADIKA <koushikeyav@gmail.com>
2026-08-24 23:24:21 +0530

    Time: 15 ms (34.78%) | Memory: 12.3 MB (97.57%) - LeetSync

---
+++ Leetcode/728-self-dividing-numbers/self-dividing-numbers.py
@@ -0,0 +1,18 @@
+class Solution(object):
+
+    def selfDividingNumbers(self,left, right):
+        list2=[]
+        while(left<=right):
+            original=left
+            temp=left
+            content=True
+            while(temp>0):
+                digit=temp%10
+                if(digit==0):
+                     content=False
+                     break
+                if(original%digit!=0):content=False
+                temp//=10
+            if(content==True):list2.append(left)
+            left+=1
+        return list2
