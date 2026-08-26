515d376a2d0ea2211852c65fbb7c5f14b5902d1b
KOUSHIKEYA VADIKA <koushikeyav@gmail.com>
2026-08-26 12:17:44 +0530

    Time: 16 ms (49.70%) | Memory: 12.5 MB (15.18%) - LeetSync

---
+++ Leetcode/1630-count-odd-numbers-in-an-interval-range/count-odd-numbers-in-an-interval-range.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def Odd(self,k):
+        if(k%2!=0):
+            return True
+        else :
+            return False
+    def countOdds(self, low, high):
+        """
+        :type low: int
+        :type high: int
+        :rtype: int
+        """
+        x=self.Odd(low)
+        y=self.Odd(high)
+        count=0
+
+        if(low<=10**9 and low>=0 and high<=10**9 and high>=0 ):
+            if(x==True and y==True):
+                count=((high-low)/2)+1
+            elif(x==True and y==False):
+                count=((high-low)+1)//2
+            elif(x==False and y==False):
+                count=(high-low)//2
+            elif(x==False and y==True):
+                count=((high-low)//2)+1
+        return count
+            
+                
+
+        return count
+
+            
+            
+
+            
