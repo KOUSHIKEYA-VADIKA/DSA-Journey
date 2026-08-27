ebcc94dcfdeedb0f5c0777269a464c6f5748dff6
KOUSHIKEYA VADIKA <koushikeyav@gmail.com>
2026-08-27 13:57:35 +0530

    Time: 11 ms (72.14%) | Memory: 14.4 MB (22.38%) - LeetSync

---
+++ Leetcode/1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def sortedSquares(self, nums):
+        l=[]
+        for i in range(len(nums)):
+            l.append(nums[i]**2)
+        l.sort()
+        return l
+        
+
+       
