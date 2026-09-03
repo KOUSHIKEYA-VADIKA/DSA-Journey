2a01573c11e92bf81eac1bad904981f568a18a1e
KOUSHIKEYA VADIKA <koushikeyav@gmail.com>
2026-09-03 22:42:15 +0530

    Time: 10 ms (93.85%) | Memory: 14.1 MB (21.72%) - LeetSync

--- Leetcode/125-valid-palindrome/valid-palindrome.py
+++ Leetcode/125-valid-palindrome/valid-palindrome.py
@@ -7,14 +7,11 @@ class Solution(object):
         s=re.sub(r'[^a-zA-Z0-9]', '', s).lower()
         l=0
         r=len(s)-1
-        if(s==""):
-            return True
+        if(s==""):return True
         flag=False
         while(l<=r):
-            if(s[l]==s[r]):
-                flag=True
-            else :
-                return False
+            if(s[l]==s[r]):flag=True
+            else :return False
             l+=1
             r-=1
         if(flag==True):return True
