aa782851fda61f1b3a410dd1388a8090ce1c5f87
KOUSHIKEYA VADIKA <koushikeyav@gmail.com>
2026-09-03 22:41:37 +0530

    Time: 11 ms (92.59%) | Memory: 14 MB (21.72%) - LeetSync

---
+++ Leetcode/125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        s=re.sub(r'[^a-zA-Z0-9]', '', s).lower()
+        l=0
+        r=len(s)-1
+        if(s==""):
+            return True
+        flag=False
+        while(l<=r):
+            if(s[l]==s[r]):
+                flag=True
+            else :
+                return False
+            l+=1
+            r-=1
+        if(flag==True):return True
+        else:return False
+
